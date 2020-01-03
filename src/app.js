395c1715a026d5b6e8fd39abdfe54044cc37b9c1
Paula Jędruszczak <paula.jedruszczak@gmail.com>
2020-01-03 14:59:06 +0100

    style: minor code improvement due to PR suggestion

--- src/app.js
+++ src/app.js
@@ -4,26 +4,22 @@ import $ from 'cash-dom';
 
 export class App {
   initializeApp() {
-    let self = this;
+    $('.load-username').on('click', () => {
+      const userName = $('.username.input').val();
 
-    $('.load-username').on('click', function (e) {
-      let userName = $('.username.input').val();
-
-      fetch('https://api.github.com/users/' + userName)
-        .then((response)=> {response.json})
-        .then(function (body) {
-          self.profile = body;
-          self.update_profile();
-        })
-
-    })
-
+      fetch(`https://api.github.com/users/${userName}`)
+        .then((response) => {
+          const body = response.json;
+          this.profile = body;
+          this.updateProfile();
+        });
+    });
   }
 
-  update_profile() {
-    $('#profile-name').text($('.username.input').val())
-    $('#profile-image').attr('src', this.profile.avatar_url)
-    $('#profile-url').attr('href', this.profile.html_url).text(this.profile.login)
-    $('#profile-bio').text(this.profile.bio || '(no information)')
+  updateProfile() {
+    $('#profile-name').text($('.username.input').val());
+    $('#profile-image').attr('src', this.profile.avatar_url);
+    $('#profile-url').attr('href', this.profile.html_url).text(this.profile.login);
+    $('#profile-bio').text(this.profile.bio || '(no information)');
   }
 }
